e8a6eca382a843191b85894687ccc9bbeab770e9
TaMeR <gruessle@gmail.com>
2022-03-06 15:11:48 +0300

    init

---
+++ matter.go
@@ -0,0 +1,25 @@
+// Package matter provides TOML frontmatter or endmatter unmarshalling.
+package matter
+
+import (
+	"bytes"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Unmarshal parses TOML frontmatter or endmatter and returns the content. 
+// When no front or endmatter delimiters are present the original content is returned.
+func Unmarshal(b []byte, v interface{}) (content []byte, err error) {
+	// Delimiter
+	var delim = []byte("+++")
+
+	parts := bytes.SplitN(b, delim, 3)
+	
+	if len(parts[0]) > len(parts[2]) {
+		content = parts[0]
+	}else{
+		content = parts[2]
+	}
+	_, err = toml.Decode(string(parts[1]), v)
+	return
+}
---
+++ matter_test.go
@@ -0,0 +1,55 @@
+package matter_test
+
+import (
+	"testing"
+	"fmt"
+	"matter"
+)
+
+type front struct {
+	Name string 
+	Tags []string 
+}
+
+var inputEnd = `
+
+Rest of the content.
+
++++
+name = "endmatter"
+tags = ['input', 'end']
++++
+
+`
+
+var inputFront = `
+
++++
+name = "frontmatter"
+tags = ['input', 'front']
++++
+
+Rest of the content.
+
+
+`
+
+func TestFront(t *testing.T){
+	ty := new(front)
+	res, err := matter.Unmarshal([]byte(inputFront), ty)
+	if err != nil {
+		t.Error( err)
+	}
+	fmt.Printf("\nres: %s\n", res)
+	fmt.Printf("ty: %s\n", ty)
+}
+
+func TestEnd(t *testing.T){
+	ty := new(front)
+	res, err := matter.Unmarshal([]byte(inputEnd), ty)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("\nres: %s\n", res)
+	fmt.Printf("ty: %s\n", ty)
+}
