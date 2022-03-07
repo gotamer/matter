b95515651c09988ae03870913baf0411e5801859
TaMeR <gruessle@gmail.com>
2022-03-07 19:19:29 +0300

    fixed bug when input has no matter at all

--- matter.go
+++ matter.go
@@ -7,19 +7,22 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-// Unmarshal parses TOML frontmatter or endmatter and returns the content. 
+// Unmarshal parses TOML frontmatter or endmatter and returns the content.
 // When no front or endmatter delimiters are present the original content is returned.
 func Unmarshal(b []byte, v interface{}) (content []byte, err error) {
 	// Delimiter
 	var delim = []byte("+++")
 
 	parts := bytes.SplitN(b, delim, 3)
-	
-	if len(parts[0]) > len(parts[2]) {
+	if len(parts) == 1 {
 		content = parts[0]
-	}else{
+	} else if len(parts[0]) > len(parts[2]) {
+		content = parts[0]
+	} else {
 		content = parts[2]
 	}
 	_, err = toml.Decode(string(parts[1]), v)
 	return
 }
+
+// func Marshal(v interface{}) (content []byte, err error) {}
